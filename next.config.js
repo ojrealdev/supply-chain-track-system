c4c1485c986d96d2016496d424c670ff9a2461f4
soojwang <soojwang@safaricom.co.ke>
2023-08-01 16:16:26 +0300

    modified next bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,17 +1,20 @@
-/* eslint-disable import/no-extraneous-dependencies */
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
+let withBundleAnalyzer = {};
+
+try {
+	// Try to load the bundle analyzer if it's available
+	withBundleAnalyzer = require('@next/bundle-analyzer')({
+		enabled: process.env.ANALYZE === 'true',
+	});
+} catch (error) {
+	console.warn('Bundle analyzer not available, continuing without it');
+}
 
 module.exports = withBundleAnalyzer({
-  eslint: {
-    dirs: ['.'],
-  },
-  poweredByHeader: false,
-  trailingSlash: true,
-  basePath: '',
-  // The starter code load resources from `public` folder with `router.basePath` in React components.
-  // So, the source code is "basePath-ready".
-  // You can remove `basePath` if you don't need it.
-  reactStrictMode: true,
+	eslint: {
+		dirs: ['.'],
+	},
+	poweredByHeader: false,
+	trailingSlash: true,
+	basePath: '',
+	reactStrictMode: true,
 });
