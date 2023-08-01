4083ecd5a91d481b575bc9008f54137682fe893b
soojwang <soojwang@safaricom.co.ke>
2023-08-01 15:17:54 +0300

    created dockerfile

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,4 +1,4 @@
-const baseUrl = 'http://localhost:3001';
-// const baseUrl = 'http://194.233.168.101:8080';
+// const baseUrl = 'http://localhost:3001';
+const baseUrl = 'http://194.233.168.101:8081';
 
 export { baseUrl };
